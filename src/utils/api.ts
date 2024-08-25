1ec424075b914f915469f2657803076d911de050
Wail Solaiman <wailsolaiman@hotmail.com>
2024-08-25 17:43:14 +0200

    Added new interface to define fetched data types

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,30 @@
 const OPEN_WEATHER_API_KEY = '2fdcb78279a1aa7def127494a6c35ac0'
 
-export async function fetchOpenWeatherData(city: string) {
+export interface OpenWeatherData {
+	name: string
+	main: {
+		feels_like: number
+		humidity: number
+		pressure: number
+		temp: number
+		temp_max: number
+		temp_min: number
+	}
+	weather: {
+		description: string
+		icon: string
+		id: number
+		main: string
+	}[]
+	wind: {
+		deg: number
+		speed: number
+	}
+}
+
+export async function fetchOpenWeatherData(
+	city: string
+): Promise<OpenWeatherData> {
 	const res = await fetch(
 		`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
 	)
@@ -9,6 +33,6 @@ export async function fetchOpenWeatherData(city: string) {
 		throw new Error('City not found!')
 	}
 
-	const data = await res.json()
+	const data: OpenWeatherData = await res.json()
 	return data
 }
