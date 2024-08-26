7fc5ab9835dd62f20a68c66ca8696016fd14e2fa
Wail Solaiman <wailsolaiman@hotmail.com>
2024-08-26 05:28:27 +0200

    Update storage to get and set new options for weather temperature scale

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,11 +1,19 @@
+import { OpenWeatherTempScale } from './api'
+
+export interface LocalStorageOptions {
+	tempScale: OpenWeatherTempScale
+}
+
 export interface LocalStorage {
 	cities?: string[]
+	options?: LocalStorageOptions
 }
 
 export type LocalStorageKeys = keyof LocalStorage
 
 export function setStoredCities(cities: string[]): Promise<void> {
 	const citiesLs: LocalStorage = { cities }
+
 	return new Promise((resolve) => {
 		chrome.storage.local.set(citiesLs, () => resolve())
 	})
@@ -13,9 +21,32 @@ export function setStoredCities(cities: string[]): Promise<void> {
 
 export function getStoredCities(): Promise<string[]> {
 	const keys: LocalStorageKeys[] = ['cities']
+
 	return new Promise((resolve) => {
 		chrome.storage.local.get(keys, (res: LocalStorage) =>
 			resolve(res.cities ?? [])
 		)
+	})
+}
+
+export function setStoredOptions(options: LocalStorageOptions): Promise<void> {
+	const optionsLs: LocalStorage = {
+		options,
+	}
+
+	return new Promise((resolve) => {
+		chrome.storage.local.set(optionsLs, () => {
+			resolve()
+		})
+	})
+}
+
+export function getStoredOptions(): Promise<LocalStorageOptions> {
+	const keys: LocalStorageKeys[] = ['options']
+
+	return new Promise((resolve) => {
+		chrome.storage.local.get(keys, (res: LocalStorage) => {
+			resolve(res.options)
+		})
 	})
 }
